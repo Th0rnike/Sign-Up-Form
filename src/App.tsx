7718adbe391905a4a264d9b72c5a5730e8b0e40b
Bald Eagle <tsereteli.t02@gmail.com>
2024-03-08 17:14:53 +0400

    project initialized and ready to write functionality

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Learn to code by watching others</h1>
+      <p>
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </p>
+      <div>
+        <div>
+          <h3>Try it free 7 days then $20/mo. thereafter</h3>
+        </div>
+        <form>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">CLAIM YOUR FREE TRIAL</button>
+          <p>
+            By clicking the button, you are agreeing to our Terms and Services
+          </p>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
